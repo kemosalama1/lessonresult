525cba33d8dd9113f842723b29b02bf21298a9bd
kemosalama1 <90480321+kemosalama1@users.noreply.github.com>
2021-09-10 23:05:36 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+   function natiga(){
+            
+            if(document.kemo.user.value=="40"){
+               document.getElementById("demo1").innerHTML="الاسم : كريم عبد السلام"
+                document.getElementById("demo2").innerHTML="الدرجة : 20"
+                document.getElementById("demo3").innerHTML="الدرجة النهائية : 25"
+                   
+                }
+       
+        }
